36ddf1bb54f2ca25bcafdf675fe2a3650681ea80
Paweł Gąsiewski <pawel.gasiewski@student.put.poznan.pl>
2020-04-21 22:54:59 +0200

    Won't make it any better today i guess. Added methods as states execution.

--- source/packet.cpp
+++ source/packet.cpp
@@ -48,114 +48,33 @@ void Packet::Execute()
     std::string pid = std::to_string(this->packet_id_);
     switch (state_)
     {
-    case State::CREATED:  //State that packet process is in just after being created
+    case State::CREATED:
     {
-      logger_->Info("Packet of ID : " + pid + " has been created.");
-      state_ = State::WAITING;  //Automatically we change state to WAITING, because here we decide what to do next
-      if (CurrentTX->BufferEmpty()) //Checking if TX's packet buffer is empty or if it contains any packets
-      {   //If the buffer is empty, packet is immediately set as TX's current and transmitted 
-        logger_->Info("Buffer of TX of ID : " + std::to_string(CurrentTX->GetTXID()) + " is empty. Packet passed.");
-        CurrentTX->SetTXPacket(this);
-      }
-      else
-      {   //If the buffer is NOT empty, the packet is put at the end of the TX's FIFO queue
-        logger_->Info("Buffer of TX of ID : " + std::to_string(CurrentTX->GetTXID()) + " not empty. Packet put in the buffer.");
-        CurrentTX->PushToBuffer(this);
-      }
+      this->StateCreated();
     }
     case State::WAITING:
     {
-        //Here, the packet checks if the channel is busy, automatically incrementing "t" parameter - counter of channel checks
-      ++this->t_;
-      if (this->network_->channel_->ChannelBusy() == true)
-      {
-        //If channel busy, state is not being changed, packet should wait for next channel check.
-        this->network_->channel_->SetBusy(true); //Channel set to busy if we start transmitting
-        logger_->Info("Channel busy, packet of ID : " + pid + " is still waiting.");
-        this->WaitCP();
-      }
-      else
-      {
-        //If channel ain't busy, the packet is passed to channet, simultaneously "t" counter resters back to 0.
-        logger_->Info("Channel free, packet of ID : " + pid + " passed to channel.");
-        this->t_ = 0;
-        state_ = State::SENT;
-      }
+      this->StateWaiting();
     }
     case State::SENT:
     {
-       //Packet is pushed to the channel
-      this->network_->channel_->PushPacketToChannel(this);
-      state_ = State::IN_CHANNEL;
+      this->StateSent();
     }
     case State::IN_CHANNEL:
     {
-      //Packet is inside of the channel (it's in channel's vector container for packets too)
-      this->WaitCTP();  //Trasmission time should be added to global time counter
-      if (this->CheckForErrors(this->network_->channel_)) //Automatically occurance of errors IN CHANNEL is checked
-        this->error_ = true;
-      if (this->CheckForCollision(this->network_->channel_)) //Same goes for collisions (more than one packet in channel at the same moment)
-        this->collision_ = true;
-      //network_->channel_->RemovePacketFromChannel();
-      logger_->Info("Packet of ID : " + pid + " passed through channel, REMOVED from channel.");
-      this->network_->channel_->RemovePacketFromChannel();  //Packet is removed from channel's packet container
-      this->network_->channel_->SetBusy(false); //Channel set to free again
-      state_ = State::RECEIVED;
+      this->StateInChannel();
     }
     case State::RECEIVED:
     {
-      //Packet reaches it's destination - the receiver
-      CurrentRX->SetRXPacket(this);   //As packet leaves the channel it's set as a current packet serviced by particular RX
-      logger_->Info("Packet of ID : " + pid + " received by RX of ID : " + std::to_string(CurrentRX->GetRXID()));
-      if ((this->error_ == true) || (this->collision_ == true))
-      {
-        //If error/collisions flags were set in channel - ack flag is set to false and the relay is needed
-        logger_->Info("Packet of ID : " + pid + " contains errors. ACK FALSE.");
-        ack_ = false;
-        retransmission_ = true;
-      }
-      else
-      {
-        //If packet was claimed successfully ack flag set to true
-        logger_->Info("Packet of ID : " + pid + " clear. ACK TRUE.");
-        ack_ = true;
-      }
-      state_ = State::SENT_BACK;
+      this->StateReceived();
     }
     case State::SENT_BACK:
     {
-      //Packet is sent back to TX throught channel
-      network_->channel_->PushPacketToChannel(this);
-      //TODO - Should channel be set as busy during ACK feedback? Also Ph.D Sroka said to ask about that.
-      this->WaitCTIZ(); //ACK transmission time to TX is being waited
-      logger_->Info("Packet of ID : " + pid + " returning with ACK flag set through the channel.");
-      network_->channel_->RemovePacketFromChannel(); //Packet removed from channel again.
-      state_ = State::CHECK;
+      this->StateSentBack();
     }
     case State::CHECK:
     {
-      //Packet is set as currently serviced in TX and it's flags are being checked
-      CurrentTX->SetTXPacket(this);
-      if ((this->ack_ == false) && (this->retransmission_ == true))
-      {
-        //As my channel availability algorithim (A4) says - if retransmission is needed here we go: 
-        logger_->Info("Transmission of packet of ID : " + pid + " failed. Necessary retransmission.");
-        ++this->r_; //"r" counter incremented because it enumerates number of retransmissions 
-        if (this->r_ < network_->kMaxRetransmissions) //If retransmissions peak not reached - one time CRP time is waited
-        {
-          //The packet state changed back to waiting, because after first channel check, it should wait CP time, not CRP again.
-          WaitCRP();
-          state_ = State::WAITING;
-        }
-        else
-          //If ack flag set to true, delete packet and output a success transmission message.
-          delete this;
-      }
-      else
-      {
-        logger_->Info("Packet of ID : " + pid + " transmitted successfully");
-        delete this;
-      }
+      this->StateCheck();
     }
     }
   }
@@ -164,6 +83,132 @@ void Packet::Execute()
 void Packet::Activate(double time)
 {
 }
+
+void Packet::StateCreated()
+{
+  TX* CurrentTX = this->network_->GetTX(this->devices_id_);
+  std::string pid = std::to_string(this->packet_id_);
+  logger_->Info("Packet of ID : " + pid + " has been created.");
+  state_ = State::WAITING;  //Automatically we change state to WAITING, because here we decide what to do next
+  if (CurrentTX->BufferEmpty()) //Checking if TX's packet buffer is empty or if it contains any packets
+  {   //If the buffer is empty, packet is immediately set as TX's current and transmitted 
+    logger_->Info("Buffer of TX of ID : " + std::to_string(CurrentTX->GetTXID()) + " is empty. Packet passed.");
+    CurrentTX->SetTXPacket(this);
+  }
+  else
+  {   //If the buffer is NOT empty, the packet is put at the end of the TX's FIFO queue
+    logger_->Info("Buffer of TX of ID : " + std::to_string(CurrentTX->GetTXID()) + " not empty. Packet put in the buffer.");
+    CurrentTX->PushToBuffer(this);
+  }
+}
+
+void Packet::StateWaiting()
+{
+  //Here, the packet checks if the channel is busy, automatically incrementing "t" parameter - counter of channel checks
+  std::string pid = std::to_string(this->packet_id_);
+  ++this->t_;
+  if (this->network_->channel_->ChannelBusy() == true)
+  {
+    //If channel busy, state is not being changed, packet should wait for next channel check.
+    this->network_->channel_->SetBusy(true); //Channel set to busy if we start transmitting
+    logger_->Info("Channel busy, packet of ID : " + pid + " is still waiting.");
+    this->WaitCP();
+  }
+  else
+  {
+    //If channel ain't busy, the packet is passed to channet, simultaneously "t" counter resters back to 0.
+    logger_->Info("Channel free, packet of ID : " + pid + " passed to channel.");
+    this->t_ = 0;
+    state_ = State::SENT;
+  }
+}
+
+void Packet::StateSent()
+{
+  //Packet is pushed to the channel
+  this->network_->channel_->PushPacketToChannel(this);
+  state_ = State::IN_CHANNEL;
+}
+
+void Packet::StateInChannel()
+{
+  //Packet is inside of the channel (it's in channel's vector container for packets too)
+  std::string pid = std::to_string(this->packet_id_);
+  this->WaitCTP();  //Trasmission time should be added to global time counter
+  if (this->CheckForErrors(this->network_->channel_)) //Automatically occurance of errors IN CHANNEL is checked
+    this->error_ = true;
+  if (this->CheckForCollision(this->network_->channel_)) //Same goes for collisions (more than one packet in channel at the same moment)
+    this->collision_ = true;
+  //network_->channel_->RemovePacketFromChannel();
+  logger_->Info("Packet of ID : " + pid + " passed through channel, REMOVED from channel.");
+  this->network_->channel_->RemovePacketFromChannel();  //Packet is removed from channel's packet container
+  this->network_->channel_->SetBusy(false); //Channel set to free again
+  state_ = State::RECEIVED;
+}
+
+void Packet::StateReceived()
+{
+  //Packet reaches it's destination - the receiver
+  std::string pid = std::to_string(this->packet_id_);
+  RX* CurrentRX = this->network_->GetRX(this->devices_id_);
+  CurrentRX->SetRXPacket(this);   //As packet leaves the channel it's set as a current packet serviced by particular RX
+  logger_->Info("Packet of ID : " + pid + " received by RX of ID : " + std::to_string(CurrentRX->GetRXID()));
+  if ((this->error_ == true) || (this->collision_ == true))
+  {
+    //If error/collisions flags were set in channel - ack flag is set to false and the relay is needed
+    logger_->Info("Packet of ID : " + pid + " contains errors. ACK FALSE.");
+    ack_ = false;
+    retransmission_ = true;
+  }
+  else
+  {
+    //If packet was claimed successfully ack flag set to true
+    logger_->Info("Packet of ID : " + pid + " clear. ACK TRUE.");
+    ack_ = true;
+  }
+  state_ = State::SENT_BACK;
+}
+
+void Packet::StateSentBack()
+{
+  //Packet is sent back to TX throught channel
+  std::string pid = std::to_string(this->packet_id_);
+  network_->channel_->PushPacketToChannel(this);
+  //TODO - Should channel be set as busy during ACK feedback? Also Ph.D Sroka said to ask about that.
+  this->WaitCTIZ(); //ACK transmission time to TX is being waited
+  logger_->Info("Packet of ID : " + pid + " returning with ACK flag set through the channel.");
+  network_->channel_->RemovePacketFromChannel(); //Packet removed from channel again.
+  state_ = State::CHECK;
+}
+
+void Packet::StateCheck()
+{
+  //Packet is set as currently serviced in TX and it's flags are being checked
+  TX* CurrentTX = this->network_->GetTX(this->devices_id_);
+  std::string pid = std::to_string(this->packet_id_);
+  CurrentTX->SetTXPacket(this);
+  if ((this->ack_ == false) && (this->retransmission_ == true))
+  {
+    //As my channel availability algorithim (A4) says - if retransmission is needed here we go: 
+    logger_->Info("Transmission of packet of ID : " + pid + " failed. Necessary retransmission.");
+    ++this->r_; //"r" counter incremented because it enumerates number of retransmissions 
+    if (this->r_ < network_->kMaxRetransmissions) //If retransmissions peak not reached - one time CRP time is waited
+    {
+      //The packet state changed back to waiting, because after first channel check, it should wait CP time, not CRP again.
+      WaitCRP();
+      state_ = State::WAITING;
+    }
+    else
+      //If ack flag set to true, delete packet and output a success transmission message.
+      delete this;
+  }
+  else
+  {
+    logger_->Info("Packet of ID : " + pid + " transmitted successfully");
+    delete this;
+  }
+}
+
 
 bool Packet::CheckForErrors(Channel* channel)
 {
--- source/packet.h
+++ source/packet.h
@@ -3,7 +3,7 @@
 #include "process.h"
 #include "logger.h"
 
-class Channel;              //Necessary includes to avoid Constructor replacement 
+class Channel;              //Necessary includes to avoid Constructor replacement & conflict
 class WirelessNetwork;
 
 class Packet : public Process //Packet is the process so it inherits from Process
@@ -17,9 +17,9 @@ public:
     IN_CHANNEL,
     RECEIVED,
     SENT_BACK,
-    CHECK,
-    RETRANSMISSION
+    CHECK
   };
+
   Packet(uint32_t did, WirelessNetwork* network);
   ~Packet();
   void WaitCRP();                              //Waits the CRP time during retransmission
@@ -27,11 +27,27 @@ public:
   void WaitCTP();                              //Waits CTP time - time of transmission
   void WaitCTIZ();                             //Waits CTIZ time - time of ACK transmission
   void Execute() override;                     //Overrided excecute method for our process
-  void Activate(double time) override;         //Overrided activate method which is 
+  void Activate(double time) override;         //Overrided activate method which is
+  double GenerateR();                          //Generates R random variable depending on "r" counter value
+  double GenerateT();                          //Generates T random variable depending on "t" channel checks counter
   bool CheckForErrors(Channel* channel);       //Checks if packet run in some errors during transmission
   bool CheckForCollision(Channel* channel);    //Checks if collision occured during transmission
 
+  //------------------------==+ STATE SPECIFIC METHODS +==--------------------------
+  // I added these methods just to make the code more structured and easier to read
+  void StateCreated();
+  void StateWaiting();
+  void StateSent();
+  void StateInChannel();
+  void StateReceived();
+  void StateSentBack();
+  void StateCheck();
+  //--------------------------------------------------------------------------------
+
+
+
 private:
+  State state_;                   //Enum class object to implement the phases of process
   WirelessNetwork* network_;      //Pointer to wirelessnetwork object, necessary for whole implementation
   Logger* logger_;                //Our logger object
   uint32_t packet_id_;            //Very own ID of the packet
@@ -40,7 +56,8 @@ private:
   uint32_t t_;                    //Channel check coutner
   double T_;                      //Random variable T value
   double R_;                      //Random variable R value
-  State state_;                   //Enum class object to implement the phases of process
+  double cp_time_;                //Value of current packet's CP time
+  double crp_time_;               //Value of current packet's CRP time
   bool error_;                    //Error occured flag
   bool ack_;                      //ACK flag
   bool active_;                   //Active flag to execute the process
