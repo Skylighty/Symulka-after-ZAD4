0a9a971f3f4ac046f80ce6fdc15be1f846ea5cc4
Skylighty <48515992+Skylighty@users.noreply.github.com>
2020-05-20 14:53:57 +0200

    Got it to work, but not as good as i want to.

---
+++ cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/source/Simulation.cpp" "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/source/Simulation.cpp.obj"
+  "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/source/channel.cpp" "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/source/channel.cpp.obj"
+  "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/source/generator.cpp" "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/source/generator.cpp.obj"
+  "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/source/logger.cpp" "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/source/logger.cpp.obj"
+  "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/source/main.cpp" "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/source/main.cpp.obj"
+  "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/source/packet.cpp" "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/source/packet.cpp.obj"
+  "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/source/process.cpp" "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/source/process.cpp.obj"
+  "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/source/rx.cpp" "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/source/rx.cpp.obj"
+  "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/source/tx.cpp" "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/source/tx.cpp.obj"
+  "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/source/wireless_network.cpp" "E:/!_STUDIA_!/SEMESTR 6/LABORKI/!Symulka_NOWA/cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/source/wireless_network.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../source"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/_Symulka_NOWA.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_Symulka_NOWA.dir/source/Simulation.cpp.obj"
+  "CMakeFiles/_Symulka_NOWA.dir/source/channel.cpp.obj"
+  "CMakeFiles/_Symulka_NOWA.dir/source/generator.cpp.obj"
+  "CMakeFiles/_Symulka_NOWA.dir/source/logger.cpp.obj"
+  "CMakeFiles/_Symulka_NOWA.dir/source/main.cpp.obj"
+  "CMakeFiles/_Symulka_NOWA.dir/source/packet.cpp.obj"
+  "CMakeFiles/_Symulka_NOWA.dir/source/process.cpp.obj"
+  "CMakeFiles/_Symulka_NOWA.dir/source/rx.cpp.obj"
+  "CMakeFiles/_Symulka_NOWA.dir/source/tx.cpp.obj"
+  "CMakeFiles/_Symulka_NOWA.dir/source/wireless_network.cpp.obj"
+  "_Symulka_NOWA.exe"
+  "_Symulka_NOWA.exe.manifest"
+  "_Symulka_NOWA.pdb"
+  "lib_Symulka_NOWA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_Symulka_NOWA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
