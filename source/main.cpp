423c9d6aaf0e110a82b68974edd8eadf70991527
Skylighty <48515992+Skylighty@users.noreply.github.com>
2020-05-20 14:55:06 +0200

    Revert "Got it to work, have some worries though."
    This reverts commit 5c965aded5bcef23700de26563439b182359ac5a.

--- source/main.cpp
+++ source/main.cpp
@@ -2,9 +2,6 @@
 #include "wireless_network.h"
 #include <iostream>
 #include <string>
-#include <vector>
-#include <queue>
-#include <fstream>
 #include <memory>
 
 void Mode(Simulation* sim){
@@ -20,36 +17,9 @@ void Mode(Simulation* sim){
     sim->RunAsSteps(simtime);
 }
 
-void LoadSeeds(std::vector<std::queue<int>> &svector)
-{
-  std::string line;
-  std::fstream seeds;
-  seeds.open("./seeds.txt");
-  if (seeds.is_open())
-  {
-    for(int i = 0; i < 4; ++i)
-    {
-      std::queue<int> sq;
-      for(int j = 0; j < 7; ++j)
-      {
-        std::getline(seeds, line);
-        sq.push(stoi(line));
-      }
-      svector.push_back(sq);
-      while (!sq.empty())
-        sq.pop();
-    }
-  }
-}
-
 int main(int argc, char *argv[]) {
-    //Prepare seeds for generators
-  std::vector<std::queue<int>> seeds_vector;
-    LoadSeeds(seeds_vector);
-    double L = 0.001;
-    WirelessNetwork *wireless_network = new WirelessNetwork(L, seeds_vector);
-    wireless_network->GenerateRXTX();
+    WirelessNetwork *wireless_network = new WirelessNetwork();
     Simulation* simulation = new Simulation(wireless_network);
-    simulation->Run(100000);
+    Mode(simulation);
     //simulation->Run(10000);
 }
--- source/tx.cpp
+++ source/tx.cpp
@@ -1,40 +1,12 @@
 #include "tx.h"
 
-TX::TX(int id, std::queue<int> &sq)
+TX::TX(int id)
 {
   tx_id_ = id;
   buffer_ = new std::queue<Packet *>;
   current_packet_ = nullptr;
-  seeds = sq;
-  GeneratorCGP = new Generator(GetSeed());
-  GeneratorCP = new Generator(GetSeed());
-  GeneratorCRP = new Generator(GetSeed());
-  GeneratorCTP = new Generator(GetSeed());
-  GeneratorR = new Generator(GetSeed());
-  GeneratorT = new Generator(GetSeed());
-  GeneratorTER = new Generator(GetSeed());
 }
 
 TX::~TX() {
 
 }
-
-int TX::GetSeed()
-{
-  int seed;
-  seed = seeds.front();
-  seeds.pop();
-  return seed;
-}
-
-/*void TX::InitializeGenerators()
-{
-  GeneratorCGP = new Generator(GetSeed());
-  GeneratorCP = new Generator(GetSeed());
-  GeneratorCRP = new Generator(GetSeed());
-  GeneratorCTP = new Generator(GetSeed());
-  GeneratorR = new Generator(GetSeed());
-  GeneratorT = new Generator(GetSeed());
-  GeneratorTER = new Generator(GetSeed());
-}*/
-
